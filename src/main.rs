1116dec778b48fd3f8b2deabb3c87089aa411fbd
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-09-22 18:05:49 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::{bail, Result};
 use std::fs::File;
 use std::io::prelude::*;
+use std::os::unix::fs::FileExt;
+use std::u16;
 
 fn main() -> Result<()> {
     // Parse arguments
@@ -20,7 +22,6 @@ fn main() -> Result<()> {
             file.read_exact(&mut header)?;
 
             // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            #[allow(unused_variables)]
             let page_size = u16::from_be_bytes([header[16], header[17]]);
 
             // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -28,6 +29,16 @@ fn main() -> Result<()> {
 
             // Uncomment this block to pass the first stage
             println!("database page size: {}", page_size);
+
+            let mut page_header = [0; 8];
+            file.read_exact(&mut page_header)?;
+            println!("page is leaf: {}", page_header[0] == 13);
+            let number_of_cells = u16::from_be_bytes([page_header[3], page_header[4]]);
+
+            let mut cell_pointer_bytes = vec![0; (number_of_cells * 2).into()];
+            file.read_exact(&mut cell_pointer_bytes)?;
+
+            println!("number of tables: {}", number_of_cells);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
